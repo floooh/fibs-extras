01a9197e3fbfbad425ef9112894682c3cdd37ca7
Andre Weissflog <floooh@gmail.com>
2026-03-04 18:45:53 +0100

    fix linux-threads.ts

--- linux-threads.ts
+++ linux-threads.ts
@@ -6,11 +6,12 @@ import { Configurer } from 'jsr:@floooh/fibs@^1';
 export function configure(c: Configurer) {
     c.addCmakeCodeInjector({
         name: 'linux-threads',
+        location: 'before-targets',
         fn: (project, config) => {
             let str = '';
             if (project.isLinux()) {
-                str += `set(THREADS_PREFER_PTHREAD_FLAG TRUE)\n`;
-                str += `find_package(Threads)\n`;
+                str += `set(THREADS_PREFER_PTHREAD_FLAG ON)\n`;
+                str += `find_package(Threads REQUIRED)\n`;
             }
             return str;
         }
@@ -18,7 +19,7 @@ export function configure(c: Configurer) {
     c.addTargetAttributeInjector({
         name: 'linux-threads',
         fn: (t, project, config) => {
-            if (project.isLinux()) {
+            if (project.isLinux() && (t.type() === 'plain-exe' || t.type() === 'windowed-exe')) {
                 t.addLibraries(['Threads::Threads'])
             }
         }
